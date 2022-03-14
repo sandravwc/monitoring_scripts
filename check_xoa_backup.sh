b26b570af85d6d1c6194d813a32625d8b9d1c563
sandravwc <87016178+sandravwc@users.noreply.github.com>
2022-03-14 17:31:17 +0100

    bro look at these evals

--- check_xoa_backup.sh
+++ check_xoa_backup.sh
@@ -1,28 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -x
 export PATH="$PATH:/usr/local/bin"
 
-xo-cli --list-commands >/dev/null 2>&1 || { echo "CRITICAL - please provide credentials for desired XOA server";  exit 2 ; } ; 
+xo-cli --list-commands >/dev/null 2>&1 || { 
+    printf "%s \n" "CRITICAL - please provide credentials for desired XOA server"
+    printf "%s \n" "example: xo-cli --register --expiresIn 311040000000 http://xoa.local:80 user pass"
+    exit 2
+    }
 all=0
 
 #################
-### variables ###
+### pre--exec ###
 
-while getopts :ast: yo 
+while getopts :ast:h asth 
 do
-case $yo in
-a)	all=1;;
-s)	sum=1;;
-t)	fetch_interval=$(($OPTARG*60*1000));;
-?)	echo "alksdjlaksdj"
-	exit 2
-	;;
+case ${asth} in
+a)  all=1;;
+s)  sum=1;;
+t)  fetch_interval=$((OPTARG*60*1000));;
+h)  HELP=1;;
+?)  echo "stop using invalid options, man."
+    exit 2
+    ;;
 esac
 done
 
-today=`date +"%U_%d-%m-%y"`
-week=`date +"%U"`
-timestamp=$((`date +%s`*1000))
+help () {
+echo -en "Usage:
+\t $0 -a 
+\t print all jobs \n
+\t $0 -s
+\t print current status depicted as a sum \n
+\t \t 1   - at least 1 job success today
+\t \t 2   - at least 1 job success this week
+\t \t 4   - at least 1 job pending today
+\t \t 8   - at least 1 job pending this week
+\t \t 16  - at least 1 job interrupted today
+\t \t 32  - at least 1 job interrupted this week
+\t \t 64  - at least 1 job skipped today
+\t \t 128 - at least 1 job skipped this week
+\t \t 256 - at least 1 job failed today
+\t \t 512 - at least 1 job failed this week \n
+\t $0 -t
+\t log fetch interval in minutes \n
+\t $0 -h
+\t show this dialog \n 
+Example: $0 -t 60 -a \n"
+    exit 0
+}
+
+[[ ${HELP} == 1 ]] && help
+
+today=$(date +"%U_%d-%m-%y")
+week=$(date +"%U")
+timestamp=$(($(date +%s)*1000))
+weeko=$((timestamp-$((60*60*24*1000))))
 tmp_dir="/tmp/check_xoa_backup"
+backup_report=0 
 _su_day=0
 _su_week=0
 _pe_day=0
@@ -37,127 +72,133 @@ _fa_week=0
 #################
 ### functions ###
 
-gt_ts () {
-[[ $(echo $line | awk -F "," '{print $1}') -gt $(($timestamp-$((60*60*24*1000)))) ]] 
-}
-
-feed_logs () {
-case $line in 
-	*success*)
-		gt_ts && { su_day+=("$line"); _su_day=1 ;} || { su_week+=("$line") _su_week=2 ;}
-		;;
-	*pending*)
-		gt_ts && { pe_day+=("$line"); _pe_day=4 ;} || { pe_week+=("$line"); _pe_week=8 ;}
-		;;
-	*interrupted*)
-		gt_ts && { in_day+=("$line"); _in_day=16 ;} || { in_week+=("$line"); _in_week=32 ;}
-		;;
-	*skipped*)
-		gt_ts && { sk_day+=("$line"); _sk_day=64 ;} || { sk_week+=("$line"i); _sk_week=128 ;}
-		;;
-	*failure*)
-		gt_ts && { fa_day+=("$line"); _fa_day=256 ;} || { fa_week+=("$line"); _fa_week=512 ;}
-		;;
-esac
-}
 print_jobs () {
-	echo -n "$(date -d @$(($(echo $job | awk -F "," '{print $1}')/1000)) +%d-%m-%y-%H:%M):""{$(echo "$job" | awk -F "," '{print $2 " " $3}')} "
+    echo -n "$(date -d @$(($(echo "${job}" \
+        | awk -F "," '{print $1}')/1000)) +%d-%m-%y-%H:%M):""{$(echo "${job}" \
+            | awk -F "," '{print $2 " " $3}')} "
 }
 
+sort_logs () {
+    line_ts=$(echo "${line}" \
+        | awk -F "," '{print $1}')
+    if [[ "${line_ts}" -gt "${weeko}" ]]
+    then
+        case "${line}" in 
+            *success*)      su_day+=("${line}") && _su_day=1   ;; 
+            *pending*)      pe_day+=("${line}") && _pe_day=4   ;;
+            *interrupted*)  in_day+=("${line}") && _in_day=16  ;;
+            *skipped*)      sk_day+=("${line}") && _sk_day=64  ;;
+            *failure*)      fa_day+=("${line}") && _fa_day=256 ;;
+        esac
+    else
+        case "${line}" in 
+            *success*)      su_week+=("${line}") && _su_week=2   ;; 
+            *pending*)      pe_week+=("${line}") && _pe_week=8   ;;
+            *interrupted*)  in_week+=("${line}") && _in_week=32  ;;
+            *skipped*)      sk_week+=("${line}") && _sk_week=128 ;;
+            *failure*)      fa_week+=("${line}") && _fa_week=512 ;;            
+        esac        
+    fi
+}
 
-[[ ! -z $(ls -1 "$tmp_dir"/$((week-1))_*.log 2>/dev/null) ]] && last_fetched=0 || last_fetched=`tail -1 "$tmp_dir"/last_fetched`
+#################
+### action!!! ###
 
-if [[ $last_fetched -eq 0 ]]
+if [[ -f "$tmp_dir/$((week-1))_*.log" ]] 
 then
-	rm -f "$tmp_dir"/$((week-1))_*.log
-	fetch=10
-elif [[ $(($timestamp-$last_fetched)) -gt $fetch_interval ]] 
-	then
-		fetch=1
-	else
-		fetch=0
+    rm -f "${tmp_dir}/$((week-1))_*.log"
+    last_fetched=0
+    fetch=10
+else
+    last_fetched=(tail -1 "${tmp_dir}/last_fetched")
+    t_minus_fetch=$((timestamp-last_fetched))
+    if [[ ${t_minus_fetch} -gt "${fetch_interval}" ]]
+    then
+        fetch=1
+    else
+        fetch=0
+    fi
 fi
 
-case $fetch in
-	10)
-		xo-cli backupNg.getAllLogs  > "$tmp_dir"/"$week"_00.log && echo $timestamp >> "$tmp_dir"/last_fetched
-		;;
-	1)
-		xo-cli backupNg.getLogs after="$(($timestamp-$last_fetched))" >> "$tmp_dir"/"$today".log && echo $timestamp >> "$tmp_dir"/last_fetched
-		;;
+case ${fetch} in
+    10)
+        xo-cli backupNg.getAllLogs \
+            > "${tmp_dir}"/"${week}"_00.log
+        echo $timestamp >> "$tmp_dir"/last_fetched
+        ;;
+    1)
+        xo-cli backupNg.getLogs after="$((timestamp-last_fetched))" \
+            >> "${tmp_dir}"/"${today}".log 
+        echo ${timestamp} >> "$tmp_dir"/last_fetched
+        ;;
 esac
 
-if [[ $all -eq 1 ]]
+if [[ ${all} -eq 1 ]]
 then
-for line in `jq -j '.[] | .id,",",.status,",",.jobName,"\n" ' "$tmp_dir"/"$week"_*.log | sort | uniq`
-do
-	feed_logs
-done
+    for line in $(jq -j '.[] | .id,",",.status,",",.jobName,"\n" ' "${tmp_dir}"/"${week}"_*.log \
+        | sort | uniq )
+    do
+        sort_logs
+    done
 else
-for line in `jq -j '.[] | select (.id > "'$(($timestamp-$((60*60*24*7*1000))))'") | .id,",",.status,",",.jobName,"\n" ' "$tmp_dir"/"$week"_*.log | sort | uniq`
-do
-	feed_logs
-done
+    for line in $(jq -j '.[] | select (.id > "'${weeko}'") | .id,",",.status,",",.jobName,"\n" ' "${tmp_dir}"/"${week}"_*.log \
+        | sort | uniq )
+    do
+        sort_logs
+    done
 fi
 
-backup_sum=$(($_su_day+$_su_week+$_pe_day+$_pe_week+$_in_day+$_in_week+$_sk_day+$_sk_week+$_fa_day+$_fa_week))
-backup_report=0; backup_report=$(($_fa_day+$_fa_week))
+backup_sum=$((_su_day+_su_week+_pe_day+_pe_week+_in_day+_in_week+_sk_day+_sk_week+_fa_day+_fa_week))
+backup_report=$((_fa_day+_fa_week))
 
-[[ $sum -eq 1 ]] && echo $backup_sum
+[[ "${sum}" -eq 1 ]] && echo "${backup_sum}"
 
-
-if [[ $all -eq 1 ]]
+if [[ "${all}" -eq 1 ]]
 then
 for status in "su" "pe" "in" "sk" "fa"; do
     for time in "week" "day"; do
-        for job in `eval echo '${'$status'_'$time'[@]}'`
-			do 
-				print_jobs
-				echo
+        for job in $(eval echo '${'${status}'_'${time}'[@]}')
+            do
+                print_jobs
+                echo
 done
-	done
-		done
-else
-case $backup_report in
-	256)
-		echo -n "check backup"
-		echo -n "today: "	
-		for job in "${fa_day[@]}"
-		do
-			print_jobs
-		done
-		echo
-		exit 2
-		;;
-	512)
-		echo -n "check backup // "
-		echo -n "this week: "
-		for job in "${fa_week[@]}"
-		do
-			print_jobs
-		done
-		echo
-		exit 1
-		;;
-	768)
-		echo -n "check backup // "
-		echo -n "today: "
-
-		for job in "${fa_day[@]}"
-		do
-			print_jobs
-		done
-		echo -n "// this week: "
-		for job in "${fa_week[@]}"
-		do
-			print_jobs
-		done
-		echo
-		exit 2
-		;;
-	0)
-		echo "nice"
-		exit 0
-		;;
-esac
+    done
+        done
 fi
+
+print_stats () {
+    for job in $(eval echo '${'${1}'_'${2}'[@]}')
+    do
+        print_jobs
+    done
+}
+
+case ${backup_report} in
+    256)
+        echo -n "check backup"
+        echo -n "today: "
+        print_stats fa day
+        echo
+        exit 2
+        ;;
+    512)
+        echo -n "check backup // "
+        echo -n "this week: "
+        print_stats fa week
+        echo
+        exit 1
+        ;;
+    768)
+        echo -n "check backup // "
+        echo -n "today: "
+        print_stats fa day
+        echo -n "// this week: "
+        print_stats fa week
+        echo
+        exit 2
+        ;;
+    0)
+        echo "nice"
+        exit 0
+        ;;
+esac
